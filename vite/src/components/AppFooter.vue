5cf403ae27af20ccee589fa6026ac74977462e7a
Rik Browning <rbrowning@nanthealth.com>
2022-03-03 11:36:40 -0500

    adding vite example

---
+++ vite/src/components/AppFooter.vue
@@ -0,0 +1,29 @@
+<template>
+  <FeatherFooter :copyright="copyright">
+    <template v-slot:contact> Contact information </template>
+    <template v-slot:links>
+      <FeatherFooterLink href="#termsofuse"> Terms of Use </FeatherFooterLink>
+      <FeatherFooterLink href="#termsofuse1">
+        Privacy Policy
+      </FeatherFooterLink>
+      <FeatherFooterLink href="#termsofuse2">
+        Notice of Privacy Practices
+      </FeatherFooterLink>
+      <FeatherFooterLink href="http://google.com" target="_blank">
+        Google
+      </FeatherFooterLink>
+    </template>
+  </FeatherFooter>
+</template>
+<script lang="ts">
+import * as components from "@featherds/footer";
+import { defineComponent } from "vue";
+export default defineComponent({
+  data: () => ({
+    copyright: "Copyright © ${year} A COMPANY, Inc.  All Rights Reserved. ",
+  }),
+  components: {
+    ...components,
+  },
+});
+</script>
---
+++ vite/src/components/AppHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <FeatherAppBar :labels="labels" :content="content">
+    <template v-slot:left>
+      <FeatherAppBarLink :icon="support" title="Support" url="#" />
+    </template>
+    <template v-slot:center>
+      <FeatherAppBarLink :icon="support" title="Support" url="#" />
+    </template>
+    <template v-slot:right>
+      <FeatherAppBarLink :icon="support" title="Support" url="#" />
+    </template>
+  </FeatherAppBar>
+</template>
+<script lang="ts">
+import { FeatherAppBar, FeatherAppBarLink } from "@featherds/app-bar";
+import Support from "@featherds/icon/action/Help";
+import { defineComponent } from "vue";
+export default defineComponent({
+  data() {
+    return {
+      labels: {
+        skip: "Skip to main content",
+      },
+      content: "main",
+      support: Support,
+    };
+  },
+  components: {
+    FeatherAppBar,
+    FeatherAppBarLink,
+  },
+});
+</script>
+<style lang="scss" scoped></style>
